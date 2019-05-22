900e659348a122838904dcd43924992ebad37633
Justin Kenney <justinkenney13@gmail.com>
2019-05-22 15:52:07 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,7 +3,7 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
+
   handleMouseMove = (event) => {
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
@@ -13,7 +13,7 @@ export default class ChromeBoisDomain extends Component {
      // debugger;
      drawChromeBoiAtCoords(event.clientX, event.clientY);
   }
-  
+
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
@@ -21,26 +21,26 @@ export default class ChromeBoisDomain extends Component {
   handleToggleCycling = (event) => {
     toggleCycling()
   }
-   
+
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
+  /* if the key pressed was 's', then it should call `resize` with '-'
    */
   handleKeyPress = (event) => {
     if (event.key === "a") {
       resize('+')
     } else if (event.key === "s") {
-      resize('-') 
+      resize('-')
     }
   }
-  
+
   render() {
     return (
       <canvas
         onClick={this.handleToggleCycling}
-        onKeyPress={this.handleKeyPress} 
+        onKeyPress={this.handleKeyPress}
         onMouseMove={this.handleMouseMove}
         width='900'
         height='600'
@@ -49,4 +49,3 @@ export default class ChromeBoisDomain extends Component {
     )
   }
 }
-
